1ea747b59dfdd52a5d1f9bad4473b18fa41084c0
Rafael140 <94143650+Rafael140@users.noreply.github.com>
2023-03-03 13:57:14 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCosm7Uk4ko4S2-kkbrH5WsKk__n7zzv-Y",
+  authDomain: "app-de-stories.firebaseapp.com",
+  databaseURL: "https://app-de-stories-default-rtdb.firebaseio.com",
+  projectId: "app-de-stories",
+  storageBucket: "app-de-stories.appspot.com",
+  messagingSenderId: "507997849617",
+  appId: "1:507997849617:web:a5a92f2f258265d3db49b0"
+};
+
